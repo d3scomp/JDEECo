409baa85748609b0f5a1868e13c4dbbef66a5391
keznikl <jaroslav.keznikl@gmail.com>
2013-06-11 16:33:10 +0200

    initial version of the runtime model

---
+++ jdeeco-core-model/src/cz/cuni/mff/d3s/jdeeco/core/model/runtimemodel/ParametricKnowledgeType.java
@@ -0,0 +1,18 @@
+/**
+ */
+package cz.cuni.mff.d3s.jdeeco.core.model.runtimemodel;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Parametric Knowledge Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see cz.cuni.mff.d3s.jdeeco.core.model.runtimemodel.RuntimemodelPackage#getParametricKnowledgeType()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface ParametricKnowledgeType extends EObject {
+} // ParametricKnowledgeType
---
+++ jdeeco-core-model/src/cz/cuni/mff/d3s/jdeeco/core/model/runtimemodel/UpdateKnowledgeStructureCommand.java
@@ -0,0 +1,17 @@
+/**
+ */
+package cz.cuni.mff.d3s.jdeeco.core.model.runtimemodel;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Update Knowledge Structure Command</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see cz.cuni.mff.d3s.jdeeco.core.model.runtimemodel.RuntimemodelPackage#getUpdateKnowledgeStructureCommand()
+ * @model
+ * @generated
+ */
+public interface UpdateKnowledgeStructureCommand extends ModelUpdateCommand {
+} // UpdateKnowledgeStructureCommand
