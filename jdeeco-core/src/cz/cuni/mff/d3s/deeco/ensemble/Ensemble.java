24585c18b5cb382633cfef297502f16f16a28cf9
rima-alali <alali@d3s.mff.cuni.cz>
2013-06-12 15:16:09 +0200

    associate knowledge with timestamp

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/ensemble/Ensemble.java
+++
@@ -1,7 +0,0 @@
-package cz.cuni.mff.d3s.deeco.ensemble;
-
-import java.io.Serializable;
-
-public abstract class Ensemble implements Serializable {
-	public final static long serialVersionUID = 1L;
-}
--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/performance/IPerformanceInfo.java
+++
@@ -1,9 +0,0 @@
-package cz.cuni.mff.d3s.deeco.performance;
-
-import java.util.Calendar;
-import java.util.HashMap;
-
-public interface IPerformanceInfo {
-
-	
-}
