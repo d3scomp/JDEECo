a04cce49c6efa36941265857b9e29f47a6488d43
Tomas Bures <bures@d3s.mff.cuni.cz>
2013-10-24 23:25:33 +0200

    Some experiments with RSA and reverse engineering.

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManager.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManager.java
@@ -3,7 +3,8 @@ package cz.cuni.mff.d3s.deeco.knowledge;
 import cz.cuni.mff.d3s.deeco.model.runtime.api.Trigger;
 
 public interface KnowledgeManager {
-	//I changed the list to single KnowledgeReference for clarity - mkit
+	//XXX: MK: I changed the list to single KnowledgeReference for clarity
+	//FIXME: TB: This is not a good idea, the list of references was there on purpose - because of being able to atomically retrieve a valueset
 	Object get(Object knowledgeReference);
 	void update(Object changeSet);
 	void register(Trigger trigger);
--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeReference.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeReference.java
@@ -1,5 +1,6 @@
 package cz.cuni.mff.d3s.deeco.knowledge;
 
+// FIXME TB: I would make this a class, but I suppose you have it on your list.
 public interface KnowledgeReference {
 
 }
