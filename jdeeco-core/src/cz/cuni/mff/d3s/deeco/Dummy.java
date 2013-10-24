e2c3378ad9ce904bb9d8814ac3010540250efb3b
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2013-10-24 12:18:31 +0200

    codebase setup

---
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/Dummy.java
@@ -0,0 +1,12 @@
+package cz.cuni.mff.d3s.deeco;
+
+/**
+ * To set up the codebase and continuous integration
+ * @author Keznikl
+ *
+ */
+public class Dummy {
+	public String hello() {
+		return "hello";
+	}
+}
