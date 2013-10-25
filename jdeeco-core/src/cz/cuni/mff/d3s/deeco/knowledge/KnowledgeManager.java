4201c113c3584cddfbdebabf956289693af2283f
Michał Kit <kit@d3s.mff.cuni.cz>
2013-10-25 08:36:08 +0200

    KnowledgeManager Tests
    Commit made on behalf of Rima

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManager.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManager.java
@@ -1,11 +1,14 @@
 package cz.cuni.mff.d3s.deeco.knowledge;
 
+import java.util.List;
+
 import cz.cuni.mff.d3s.deeco.model.runtime.api.Trigger;
 
 public interface KnowledgeManager {
 	//XXX: MK: I changed the list to single KnowledgeReference for clarity
 	//FIXME: TB: This is not a good idea, the list of references was there on purpose - because of being able to atomically retrieve a valueset
-	Object get(Object knowledgeReference);
-	void update(Object changeSet);
-	void register(Trigger trigger);
+	//FIXED: MK: I understand now.
+	ValueSet get(List<KnowledgeReference> knowledgeReference);
+	void update(ChangeSet changeSet);
+ 	void register(Trigger trigger);
 }
---
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManagerImpl.java
@@ -0,0 +1,27 @@
+package cz.cuni.mff.d3s.deeco.knowledge;
+
+import java.util.List;
+
+import cz.cuni.mff.d3s.deeco.model.runtime.api.Trigger;
+
+public class KnowledgeManagerImpl implements KnowledgeManager{
+
+	@Override
+	public ValueSet get(List<KnowledgeReference> knowledgeReference) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(ChangeSet changeSet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void register(Trigger trigger) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
