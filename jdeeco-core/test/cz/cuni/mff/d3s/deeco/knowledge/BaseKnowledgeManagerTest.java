569b9038d10e44eba8815d24902fbce1b609af20
Michał Kit <kit@d3s.mff.cuni.cz>
2013-11-07 16:48:31 +0100

    Formatting fix. (.git/config changed)

---
+++ jdeeco-core/test/cz/cuni/mff/d3s/deeco/knowledge/BaseKnowledgeManagerTest.java
@@ -0,0 +1,296 @@
+package cz.cuni.mff.d3s.deeco.knowledge;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import cz.cuni.mff.d3s.deeco.model.runtime.RuntimeModelHelper;
+import cz.cuni.mff.d3s.deeco.model.runtime.api.KnowledgeChangeTrigger;
+import cz.cuni.mff.d3s.deeco.model.runtime.api.KnowledgePath;
+
+/**
+ * BaseKnowledgeManager testing.
+ * 
+ * @author Rima Al Ali <alali@d3s.mff.cuni.cz>
+ * @author Michal Kit <kit@d3s.mff.cuni.cz>
+ * 
+ */
+public class BaseKnowledgeManagerTest {
+
+	private BaseKnowledgeManager toBeTested;
+
+	@Mock
+	private TriggerListener triggerListener;
+
+	@Before
+	public void setUp() {
+		toBeTested = new BaseKnowledgeManager(new Knowledge());
+		initMocks(this);
+	}
+
+	@Test
+	public void testUpdateIntegerField() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the update for the 'number' field is passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("number");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ChangeSet toUpdate = new ChangeSet();
+		toUpdate.setValue(kp, 17);
+
+		toBeTested.update(toUpdate);
+		// THEN when accessed the number field the KnowledgeManager should
+		// return updated value
+		ValueSet result = toBeTested.get(knowledgePaths);
+		assertEquals(17, result.getValue(kp));
+	}
+
+	@Test
+	public void testInnerKnowledgeUpdate() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the update for some nested inner field is passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath(
+				"innerKnowledge", "a");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ChangeSet toUpdate = new ChangeSet();
+		toUpdate.setValue(kp, "innerAModified");
+
+		toBeTested.update(toUpdate);
+		// THEN when accessed the inner knowledge the KnowledgeManager should
+		// return updated value
+		ValueSet result = toBeTested.get(knowledgePaths);
+		assertEquals("innerAModified", result.getValue(kp));
+	}
+
+	@Test
+	public void testUpdateListField() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the update for one of the 'list' items is passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("list", "2");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ChangeSet toUpdate = new ChangeSet();
+		toUpdate.setValue(kp, 4);
+		toBeTested.update(toUpdate);
+
+		// THEN when accessed the item value the KnowledgeManager should return
+		// updated value
+		ValueSet result = toBeTested.get(knowledgePaths);
+		assertEquals(4, result.getValue(kp));
+	}
+
+	@Test
+	public void testUpdateMapField() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the update for one of the 'map' items is passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("map", "a");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ChangeSet toUpdate = new ChangeSet();
+		toUpdate.setValue(kp, 16);
+		toBeTested.update(toUpdate);
+
+		// THEN when accessed the item value the KnowledgeManager should return
+		// updated value
+		ValueSet result = toBeTested.get(knowledgePaths);
+		assertEquals(16, result.getValue(kp));
+	}
+
+	@Test
+	public void testRemovalFromList() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the removal of one of the 'list' items is passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("list", "2");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+		Object nextItemValue = toBeTested.get(knowledgePaths).getValue(kp);
+		kp = RuntimeModelHelper.createKnowledgePath("list", "1");
+		knowledgePaths.clear();
+		knowledgePaths.add(kp);
+		
+		ChangeSet toDelete = new ChangeSet();
+		toDelete.setDeleted(kp);
+		toBeTested.update(toDelete);
+
+		// THEN when accessed the deleted item index, the KnowledgeManager
+		// should return
+		// the value of the next item
+		assertEquals(nextItemValue, toBeTested.get(knowledgePaths).getValue(kp));
+	}
+
+	@Test(expected = KnowledgeNotFoundException.class)
+	public void testDeleteFromMap() throws Exception {
+		// WHEN the update method is called on the KnowledgeManager
+		// and as a ChangeSet, the removal of one of the 'map' elements is
+		// passed
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("map", "a");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ChangeSet toDelete = new ChangeSet();
+		toDelete.setDeleted(kp);
+		toBeTested.update(toDelete);
+
+		// THEN when accessed the removed element value the KnowledgeManager
+		// should throw the KnowledgeNotExistentException
+		toBeTested.get(knowledgePaths);
+	}
+
+	@Test
+	public void testGetIntegerField() throws Exception {
+		// WHEN a field is accessed from the ReadOnlyKnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("number");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ValueSet result = toBeTested.get(knowledgePaths);
+		// THEN the correct value is returned
+		assertEquals(10, result.getValue(kp));
+	}
+
+	@Test
+	public void testInnerKnowledgeGet() throws Exception {
+		// WHEN inner knowledge is accessed from the ReadOnlyKnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath(
+				"innerKnowledge", "a");
+
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ValueSet result = toBeTested.get(knowledgePaths);
+		// THEN the correct value is returned
+		assertEquals("innerA", result.getValue(kp));
+	}
+
+	@Test(expected = KnowledgeNotFoundException.class)
+	public void testNullBaseKnowledgeAccess() throws Exception {
+		toBeTested = new BaseKnowledgeManager();
+		// WHEN a field is accessed from the knowledge manager initialized with
+		// null base knowledge
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("number");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+		// THEN exception is thrown.
+		toBeTested.get(knowledgePaths);
+	}
+
+	@Test
+	public void testGetListField() throws Exception {
+		// WHEN an item of a list is accessed from the ReadOnlyKnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("list", "2");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ValueSet result = toBeTested.get(knowledgePaths);
+		// THEN the correct value is returned
+		assertEquals(3, result.getValue(kp));
+	}
+
+	@Test
+	public void testGetMapField() throws Exception {
+		// WHEN an element of a map is accessed from the
+		// ReadOnlyKnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("map", "a");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		ValueSet result = toBeTested.get(knowledgePaths);
+		// THEN the correct value is returned
+		assertEquals(1, result.getValue(kp));
+	}
+
+	@Test(expected = KnowledgeNotFoundException.class)
+	public void testNotExsistentAccess() throws Exception {
+		// WHEN a not existent entry is accessed from the
+		// ReadOnlyKnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("dummy");
+		List<KnowledgePath> knowledgePaths = new LinkedList<>();
+		knowledgePaths.add(kp);
+
+		// THEN the ReadOnlyKnowledgeManager should throw the
+		// KnowledgeNotExistentException
+		toBeTested.get(knowledgePaths);
+	}
+
+	@Test
+	public void testRegisterListener() {
+		// WHEN a listener is registered at the KnowledgeManager
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("number");
+		KnowledgeChangeTrigger trigger = RuntimeModelHelper
+				.createKnowledgeChangeTrigger();
+		trigger.setKnowledgePath(kp);
+		toBeTested.register(trigger, triggerListener);
+		// and WHEN listener's releavant knowledge is updated
+		ChangeSet toUpdate = new ChangeSet();
+		toUpdate.setValue(kp, 17);
+		toBeTested.update(toUpdate);
+		// THEN the listener is notify once.
+		verify(triggerListener).triggered(trigger);
+		verifyNoMoreInteractions(triggerListener);
+	}
+
+	@Test
+	public void testUnregisterListener() {
+		// WHEN a previously registered listener
+		KnowledgePath kp = RuntimeModelHelper.createKnowledgePath("number");
+		KnowledgeChangeTrigger trigger = RuntimeModelHelper
+				.createKnowledgeChangeTrigger();
+		trigger.setKnowledgePath(kp);
+		toBeTested.register(trigger, triggerListener);
+		// is unregistered from the KnowledgeManager
+		toBeTested.register(trigger, triggerListener);
+		// THEN it is not notified about knowledge changes any more
+		verifyNoMoreInteractions(triggerListener);
+	}
+
+	public static class Knowledge {
+		public String id;
+		public Integer number;
+		public List<Integer> list;
+		public Date date;
+		public Map<String, Integer> map;
+		public InnerKnowledge innerKnowledge;
+
+		public Knowledge() {
+			this.id = "Test";
+			this.number = 10;
+			this.list = new LinkedList<>();
+			this.list.add(1);
+			this.list.add(2);
+			this.list.add(3);
+			this.map = new HashMap<String, Integer>();
+			this.map.put("a", 1);
+			this.map.put("b", 2);
+			this.map.put("c", 3);
+			this.innerKnowledge = new InnerKnowledge("innerA", "innerB");
+		}
+	}
+
+	public static class InnerKnowledge {
+		public String a;
+		public String b;
+
+		public InnerKnowledge(String a, String b) {
+			super();
+			this.a = a;
+			this.b = b;
+		}
+	}
+
+}
--- jdeeco-core/test/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManagersViewTest.java
+++ jdeeco-core/test/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManagersViewTest.java
@@ -25,95 +25,95 @@ import cz.cuni.mff.d3s.deeco.model.runtime.api.Trigger;
  *
  */
 public class KnowledgeManagersViewTest {
-	
-	@Mock
-	private Trigger trigger;
-	@Mock
-	private ShadowsTriggerListener shadowsTriggerListener;
-	@Mock
-	private ValueSet valueSet;
-	@Mock
-	private ReadOnlyKnowledgeManager km;
+        
+        @Mock
+        private Trigger trigger;
+        @Mock
+        private ShadowsTriggerListener shadowsTriggerListener;
+        @Mock
+        private ValueSet valueSet;
+        @Mock
+        private ReadOnlyKnowledgeManager km;
  
-	private KnowledgeManagersView kmView;
+        private KnowledgeManagersView kmView;
 
-	
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		
-		// FIXME TB: Why do you have the "this." here?
-		// FIXME TB: Why don't you instantiate this mock using the annotation as the other mocks above?
-		this.kmView =mock(KnowledgeManagersView.class);
-	}
+        
+        @Before
+        public void setUp() throws Exception {
+                MockitoAnnotations.initMocks(this);
+                
+                // FIXME TB: Why do you have the "this." here?
+                // FIXME TB: Why don't you instantiate this mock using the annotation as the other mocks above?
+                this.kmView =mock(KnowledgeManagersView.class);
+        }
 
-	// FIXME TB: The tests below need to be fixed to comply to BDD.
-	
- 	@Test 
-	public void testGetOtherKnowledgeManager(){
-		// WHEN km mocks KnowledgeManager and define a Collection of ReadOnlyKnowledgeManagers that do not have km
-		this.km = mock(ReadOnlyKnowledgeManager.class);
- 		Collection<ReadOnlyKnowledgeManager> colKM = new LinkedList<ReadOnlyKnowledgeManager>();
- 		// THEN define the behavior of KnowledgeManagerView :
-		//  1- return "colKM" by calling getOthersKnowledgeManagers()
-		when(kmView.getOthersKnowledgeManagers()).thenReturn(colKM);
-		
-		// WHEN call getOthersKnowledgeManagers()
-		// THEN check the returned collection do not have km 
-		assertEquals(false, kmView.getOthersKnowledgeManagers().contains(km));
-		
-	}
-	
-	@Ignore
- 	@Test
-	public void testRegisterOthers() {
-		// WHEN define a new for trigger
-		// FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
-		this.trigger = mock(Trigger.class);
-		// THEN add the trigger(s) to a Collection
-		Collection<Trigger> colTriggers= new LinkedList<Trigger>();
-		colTriggers.add(trigger);
-		
-		// WHEN mock new TriggerListener
-		// FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
-		this.shadowsTriggerListener = mock(ShadowsTriggerListener.class);
-		// THEN define the behavior of the TriggerListener:
-		//	 the value "colTriggers" returned by getTriggers()
-		//when(triggerListener.getTriggers()).thenReturn(colTriggers);
-		
-		
-		// WHEN registering a trigger to triggerListener
-		kmView.register(trigger,shadowsTriggerListener);
-		// THEN the triggerListener adds this trigger
-		// assertEquals(true,triggerListener.getTriggers().contain(trigger));
-		
-		verifyNoMoreInteractions(trigger);
-	}
-	
-	@Ignore
- 	@Test
-	public void testUnregisterOthers() {
-		// WHEN define a new for trigger   
-		// FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
-		this.trigger = mock(Trigger.class);
-		// THEN define a Collection of Triggers without the currentTrigger
-		Collection<Trigger> colTriggers= new LinkedList<Trigger>();
-		
-		// WHEN mock new TriggerListener
-		// FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
-		this.shadowsTriggerListener = mock(ShadowsTriggerListener.class);
-		// THEN define the behavior of the TriggerListener:
-		//	 the value "colTriggers" returned by getTriggers()
-		//when(triggerListener.getTriggers()).thenReturn(colTriggers);
-		
-		
-		// WHEN registering a trigger to triggerListener
-		kmView.unregister(trigger,shadowsTriggerListener);
-		// THEN the triggerListener removes this trigger
-		// assertEquals(false,triggerListener.getTriggers().contain(trigger));
+        // FIXME TB: The tests below need to be fixed to comply to BDD.
+        
+         @Test 
+        public void testGetOtherKnowledgeManager(){
+                // WHEN km mocks KnowledgeManager and define a Collection of ReadOnlyKnowledgeManagers that do not have km
+                this.km = mock(ReadOnlyKnowledgeManager.class);
+                 Collection<ReadOnlyKnowledgeManager> colKM = new LinkedList<ReadOnlyKnowledgeManager>();
+                 // THEN define the behavior of KnowledgeManagerView :
+                //  1- return "colKM" by calling getOthersKnowledgeManagers()
+                when(kmView.getOtherKnowledgeManagers()).thenReturn(colKM);
+                
+                // WHEN call getOthersKnowledgeManagers()
+                // THEN check the returned collection do not have km 
+                assertEquals(false, kmView.getOtherKnowledgeManagers().contains(km));
+                
+        }
+        
+        @Ignore
+         @Test
+        public void testRegisterOthers() {
+                // WHEN define a new for trigger
+                // FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
+                this.trigger = mock(Trigger.class);
+                // THEN add the trigger(s) to a Collection
+                Collection<Trigger> colTriggers= new LinkedList<Trigger>();
+                colTriggers.add(trigger);
+                
+                // WHEN mock new TriggerListener
+                // FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
+                this.shadowsTriggerListener = mock(ShadowsTriggerListener.class);
+                // THEN define the behavior of the TriggerListener:
+                //         the value "colTriggers" returned by getTriggers()
+                //when(triggerListener.getTriggers()).thenReturn(colTriggers);
+                
+                
+                // WHEN registering a trigger to triggerListener
+                kmView.register(trigger,shadowsTriggerListener);
+                // THEN the triggerListener adds this trigger
+                // assertEquals(true,triggerListener.getTriggers().contain(trigger));
+                
+                verifyNoMoreInteractions(trigger);
+        }
+        
+        @Ignore
+         @Test
+        public void testUnregisterOthers() {
+                // WHEN define a new for trigger   
+                // FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
+                this.trigger = mock(Trigger.class);
+                // THEN define a Collection of Triggers without the currentTrigger
+                Collection<Trigger> colTriggers= new LinkedList<Trigger>();
+                
+                // WHEN mock new TriggerListener
+                // FIXME TB: Why do you call the mock here when the mock has been already created because of the annotation above and the call to initMocks?
+                this.shadowsTriggerListener = mock(ShadowsTriggerListener.class);
+                // THEN define the behavior of the TriggerListener:
+                //         the value "colTriggers" returned by getTriggers()
+                //when(triggerListener.getTriggers()).thenReturn(colTriggers);
+                
+                
+                // WHEN registering a trigger to triggerListener
+                kmView.unregister(trigger,shadowsTriggerListener);
+                // THEN the triggerListener removes this trigger
+                // assertEquals(false,triggerListener.getTriggers().contain(trigger));
 
-		verifyNoMoreInteractions(trigger);
+                verifyNoMoreInteractions(trigger);
 
-	}
-	
+        }
+        
 }
