e0e27a2768799ca115e39c60947eb700a186006d
Michał Kit <kit@d3s.mff.cuni.cz>
2013-11-08 09:33:16 +0100

    Tests to KnowledgeManagerViewImpl + Fixes to KnowledgeManagerViewImpl implementation

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManagerViewImpl.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/KnowledgeManagerViewImpl.java
@@ -82,8 +82,10 @@ public class KnowledgeManagerViewImpl implements KnowledgeManagersView,
 				.values()) {
 			toRemove.clear();
 			for (KnowledgeManagerTriggerListener tListener : tListeners)
-				if (tListener.getKnowledgeManager().equals(km))
+				if (tListener.getKnowledgeManager().equals(km)) {
+					km.unregister(tListener.getTrigger(), tListener);
 					toRemove.add(tListener);
+				}
 			tListeners.removeAll(toRemove);
 		}
 	}
@@ -161,7 +163,7 @@ public class KnowledgeManagerViewImpl implements KnowledgeManagersView,
 			Trigger trigger, ShadowsTriggerListener triggerListener) {
 		List<KnowledgeManagerTriggerListener> tListeners;
 		KnowledgeManagerTriggerListener tListener = new KnowledgeManagerTriggerListener(
-				km, triggerListener);
+				km, triggerListener, trigger);
 		// Update map of listenerToTriggerListeners
 		if (listenerToTriggerListeners.containsKey(triggerListener))
 			tListeners = listenerToTriggerListeners.get(triggerListener);
@@ -179,12 +181,14 @@ public class KnowledgeManagerViewImpl implements KnowledgeManagersView,
 
 		private final KnowledgeManager knowledgeManager;
 		private final ShadowsTriggerListener listener;
+		private final Trigger trigger;
 
 		public KnowledgeManagerTriggerListener(
 				KnowledgeManager knowledgeManager,
-				ShadowsTriggerListener listener) {
+				ShadowsTriggerListener listener, Trigger trigger) {
 			this.knowledgeManager = knowledgeManager;
 			this.listener = listener;
+			this.trigger = trigger;
 		}
 
 		@Override
@@ -194,6 +198,10 @@ public class KnowledgeManagerViewImpl implements KnowledgeManagersView,
 
 		public KnowledgeManager getKnowledgeManager() {
 			return knowledgeManager;
+		}
+		
+		public Trigger getTrigger() {
+			return trigger;
 		}
 	}
 
