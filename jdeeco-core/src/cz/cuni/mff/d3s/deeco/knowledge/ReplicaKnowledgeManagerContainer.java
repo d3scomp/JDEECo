d07cdc209542dd23d326ca4e933b49bf517d80ab
Tomas Bures <bures@d3s.mff.cuni.cz>
2013-11-13 20:35:33 +0100

    A small FIXME - not necessary to solve it now.

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/ReplicaKnowledgeManagerContainer.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/ReplicaKnowledgeManagerContainer.java
@@ -7,6 +7,11 @@ import java.util.List;
  *
  */
 public interface ReplicaKnowledgeManagerContainer {
+	// FIXME TB: I can imagine that instead of createReplicaFor you will need only createReplica
+	// to return you an empty knowledge manager, which you then populate using the synchronizer
+	// I think that especially in the real distributed case when you will be getting only partial
+	// data this will be more appropriate.
+	// However, for now, it may stay, as this functionality is anyway not needed for milestone #1
 	public KnowledgeManager createReplicaFor(KnowledgeManager km);
 	public KnowledgeManager removeReplica(KnowledgeManager km);
 	public List<KnowledgeManager> getReplicas();
