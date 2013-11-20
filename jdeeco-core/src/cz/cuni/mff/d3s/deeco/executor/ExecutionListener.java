09369824c61f3d00b711d424fc01e986f95a3586
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2013-11-20 13:29:42 +0100

    executor.* docs

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/executor/ExecutionListener.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/executor/ExecutionListener.java
@@ -2,9 +2,30 @@ package cz.cuni.mff.d3s.deeco.executor;
 
 import cz.cuni.mff.d3s.deeco.task.Task;
 
-//FIXME TB: The class does not have the header stating the author
-
+/**
+ * Listener interface for notifications on successful/failed execution via an {@link Executor}.
+ * 
+ * @see Executor
+ * 
+ * @author Jaroslav Keznikl <keznikl@d3s.mff.cuni.cz>
+ *
+ */
 public interface ExecutionListener {
-	public void executionCompleted( Task task );
-	public void executionFailed( Task task, Exception e );
+	/**
+	 * Called if the {@code task} was successfully executed.
+	 * 
+	 * @param task
+	 *            the task that has been successfully executed
+	 */
+	void executionCompleted( Task task );
+
+	/**
+	 * Called if execution of the {@code task} failed.
+	 * 
+	 * @param task
+	 *            the task execution of which failed
+	 * @param e
+	 *            the exception that caused the task execution to fail
+	 */
+	void executionFailed( Task task, Exception e );
 }
