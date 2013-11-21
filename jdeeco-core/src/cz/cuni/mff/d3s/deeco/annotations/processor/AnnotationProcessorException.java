7a77e69f052acba3b55cf5a9d530afa710850659
Ilias Gerostathopoulos <iliasger@gmail.com>
2013-11-21 20:50:03 +0100

    javadoc added in AnnotationProcessorException

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/annotations/processor/AnnotationProcessorException.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/annotations/processor/AnnotationProcessorException.java
@@ -1,5 +1,14 @@
 package cz.cuni.mff.d3s.deeco.annotations.processor;
 
+/**
+ * Thrown when processing of DEECo-annotated Java object(s)/class(es) fails.
+ * <p>
+ * The message should encode all relevant information (class name, method name, parameter index, ...) to pinpoint the problem. 
+ * </p>
+ * 
+ * @author Ilias Gerostathopoulos <iliasg@d3s.mff.cuni.cz>
+ *
+ */
 public class AnnotationProcessorException extends Exception {
 
 	private static final long serialVersionUID = 1L;
