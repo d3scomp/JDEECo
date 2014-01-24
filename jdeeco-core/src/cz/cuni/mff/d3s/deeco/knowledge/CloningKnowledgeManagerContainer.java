2c4534886f9204db33a9fccae6e96fb67c207c68
Michał Kit <kit@d3s.mff.cuni.cz>
2014-01-24 13:02:15 +0100

    Simulation adjustments.

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/CloningKnowledgeManagerContainer.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/knowledge/CloningKnowledgeManagerContainer.java
@@ -15,54 +15,19 @@ import java.util.List;
  * @author Michal Kit <kit@d3s.mff.cuni.cz>
  * 
  */
-public class CloningKnowledgeManagerContainer implements
-		ReplicaKnowledgeManagerContainer, LocalKnowledgeManagerContainer {
+public class CloningKnowledgeManagerContainer extends LocalKnowledgeManagerContainer implements
+		ReplicaKnowledgeManagerContainer {
 
-	protected final List<KnowledgeManager> locals;
 	protected final List<KnowledgeManager> replicas;
-	protected final List<LocalListener> localListeners;
 	protected final List<ReplicaListener> replicaListeners;
 
 	public CloningKnowledgeManagerContainer() {
-		this.locals = new LinkedList<>();
 		this.replicas = new LinkedList<>();
-		this.localListeners = new LinkedList<>();
 		this.replicaListeners = new LinkedList<>();
 	}
-
-	@Override
+	
 	public KnowledgeManager createLocal(String id) {
-		KnowledgeManager result = new CloningKnowledgeManager(id);
-		locals.add(result);
-		for (LocalListener listener : localListeners){
-			listener.localCreated(result, this);
-		}
-		return result;
-	}
-
-	@Override
-	public KnowledgeManager removeLocal(KnowledgeManager km) {
-		KnowledgeManager kmVar = null;
-		if (locals.contains(km)) {
-			locals.remove(km);
-			for (LocalListener listener : localListeners){
-				listener.localRemoved(km, this);
-			}
-			kmVar = km;
-		}
-		return kmVar;
-	}
-
-	@Override
-	public List<KnowledgeManager> getLocals() {
-		return locals;
-	}
-
-	@Override
-	public void registerLocalListener(LocalListener listener) {
-		if (!localListeners.contains(listener)){
-			localListeners.add(listener);
-		}
+		return super.createLocal(id);
 	}
 
 	@Override
