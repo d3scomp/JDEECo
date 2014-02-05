1eec0233d41dd48861435d7bae2292f87cc7aa1c
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2014-02-05 23:21:31 +0100

    - extended logging in core - list vs. individual broadcasting of KnowledgeData in core - analysis of sent vs. received vs. dropped message IDs - setting boundary condition in the demo on/off

--- jdeeco-simulation-demo/analysis/analyze_log.py
+++ jdeeco-simulation-demo/analysis/analyze_log.py
@@ -3,14 +3,14 @@ from numpy import average
 
 
 lines = []
-with open('../logs/no-boundary.log', 'r') as f:
+with open('../logs/jdeeco.log.0', 'r') as f:
     lines = f.readlines()
     
 hoplines = filter(lambda x: x.endswith('hops\n'), lines)
 hopData = []
-p = re.compile('after (\d+)ms and (\d+) hops')
+pattern = re.compile('after (\d+)ms and (\d+) hops')
 for line in hoplines:
-    m = p.search(line)
+    m = pattern.search(line)
     time = m.group(1)
     hops = m.group(2)
     hopData.append({'time': int(time), 'hops': int(hops)})
@@ -37,3 +37,36 @@ receiveLines = filter(lambda x: ', received [' in x, lines)
 # on each receive line, each received KD is followed by a ',' + there is one more in the message
 dataReceived = sum(map(lambda l: l.count(',') - 1, receiveLines)) 
 print 'KnowledgeData received:', dataReceived
+
+
+def extract_sent_id(line):
+    p = re.compile('with messageid (.\d+)')
+    m = p.search(line)
+    return m.group(1)
+sentMsgLines = filter(lambda x: 'PacketSender: Sending MSG' in x, lines)
+sentIds = set(map(extract_sent_id, sentMsgLines))
+print 'Sent messages: ', len(sentIds)
+
+def extract_dropped_ids(line):
+    p = re.compile('dropped messageids \[(.*)\]')
+    m = p.search(line)
+    ids = m.group(1)
+    if ids is not None:
+        return set(ids.split(', '))
+    else:
+        return set() 
+dropMsgLines = filter(lambda x: 'dropped messageids' in x, lines)
+droppedIds = set.union(*map(extract_dropped_ids, dropMsgLines))
+print 'Dropped messages: ', len(droppedIds)
+
+ 
+def extract_received_id(line):
+    p = re.compile('with messageid (.\d+)')
+    m = p.search(line)
+    return m.group(1)
+receivedMsgLines = filter(lambda x: 'PacketReceiver: Message completed' in x, lines)
+receivedIds = set(map(extract_received_id, receivedMsgLines))
+print 'Received messages: ', len(receivedIds)
+
+print 'Received/Sent ratio:', len(receivedIds) * 1.0 /len(sentIds)
+
