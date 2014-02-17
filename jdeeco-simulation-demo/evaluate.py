ebf07e6c374c7dc1420d5ebd27057ecbe3f778ab
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2014-02-17 18:51:43 +0100

    class-based evaluation script

--- jdeeco-simulation-demo/evaluate.py
+++ jdeeco-simulation-demo/evaluate.py
@@ -12,17 +12,15 @@ from shutil import copyfile
 import numpy as np
 import pylab
 from numpy import average
+from multiprocessing import *
 
-processes = []
-processInit = {}
-logProps = {}
-stdoutFiles = {}
+simulated = []
 
 cpus = 3
 
 def cleanup():
     timeout_sec = 5
-    for p in processes:  # list of your processes
+    for p in [s.simulation for s in simulated]:  # list of your processes
         p_sec = 0
         for second in range(timeout_sec):
             if p.poll() == None:
@@ -35,9 +33,8 @@ def cleanup():
 atexit.register(cleanup)
 root = os.path.dirname(os.path.realpath(__file__))
 
-
 class Scenario():
-    def __init__(self, nodeCnt, iterationCnt, boundaryEnabled=True, generator='simple.py'):
+    def __init__(self, nodeCnt, iterationCnt, boundaryEnabled, generator):
         self.nodeCnt = nodeCnt
         self.iterationCnt = iterationCnt
         self.boundaryEnabled = boundaryEnabled
@@ -48,13 +45,15 @@ class Scenario():
     def folder(self):
         return 'simulation-results\\%d' % (self.nodeCnt)
     def folderPath(self):
-        return root + 'simulation-results\\%d' % (self.nodeCnt)
+        return root + '\\simulation-results\\%d' % (self.nodeCnt)
     def genericResultsPath(self): 
-        return root + 'simulation-results\\results-generic-%d.csv' % self.nodeCnt
+        return root + '\\simulation-results\\results-generic-%d.csv' % self.nodeCnt
     def demoResultsPath(self): 
-        return root + 'simulation-results\\results-demo-%d.csv' % self.nodeCnt
+        return root + '\\simulation-results\\results-demo-%d.csv' % self.nodeCnt
     def neighborResultsPath(self): 
-        return root + 'simulation-results\\results-neighbors-%d.csv' % self.nodeCnt
+        return root + '\\simulation-results\\results-neighbors-%d.csv' % self.nodeCnt
+    def plotTick(self):
+        return str(self.nodeCnt)
        
     
 class ScenarioIteration:
@@ -64,10 +63,15 @@ class ScenarioIteration:
         self.iteration = iteration
         self.boundaryEnabled = boundaryEnabled
         self.generator = generator    
+        self.stdOut = None
+        self.simulation = None
+        self.demoAnalysis = None
+        self.genericAnalysis = None
+        self.neighborAnalysis = None
     def folder(self):
         return self.scenario.folder()
     def prefix(self):
-        return '%d-%d-%s-%s-' % (self.nodeCnt, self.iteration, 'b' if self.boundaryEnabled else 'n', self.generator[0])
+        return '%d-%d-%s-%s-' % (self.nodeCnt, self.iteration, 't' if self.boundaryEnabled else 'f', self.generator[0])
     def prefixPath(self):
         return root + '\\' + self.folder() + '\\' + self.prefix()
     def genericAnalysisStdoutPath(self):
@@ -81,23 +85,26 @@ class ScenarioIteration:
     def stdOutPath(self):
         return self.prefixPath() + 'stdout.log'
     def loggingPropertiesPath(self):
+        # needs to be relative path
         return self.prefix() + 'logging.properties'
     def componentCfgPath(self):
         return self.prefixPath() + 'component.cfg'
     def siteCfgPath(self):
         return self.prefixPath() + 'site.cfg'
     def omnetppPath(self):
+        # needs to be relative path
         return self.prefix() + 'omnetpp.ini'    
     def name(self):
         return self.prefix() + 'scenario'
 
 #evaluations = {4:10, 8:10, 12: 10, 16:10, 20:10}
-evaluations = {8:10, 12: 10, 16:10, 20:10, 24:10, 28:10}
+#evaluations = {8:10, 12: 10, 16:10, 20:10, 24:10, 28:10}
+evaluations = {8:3, 12: 3}
 
 # GENERATE SCENARIOS
 scenarios = []
 for nodeCnt in evaluations.keys():    
-    scenarios.append(Scenario(nodeCnt, evaluations[nodeCnt], False, 'simple.py'))
+    scenarios.append(Scenario(nodeCnt, evaluations[nodeCnt], False, 'simple'))
 
 def generate():
     print 'Generating configurations...'
@@ -106,52 +113,40 @@ def generate():
             os.makedirs(s.folderPath())
         except OSError as e:
             pass
-        for iteration in s.iterations:
-            print 'Generating ', iteration.name()
-            generateConfig(1, siteration.nodeCnt-1, 0, siteration.prefixPath(), 0)
-            
-    for nodeCnt in evaluations.keys():
-        iterations = evaluations[nodeCnt]
-        try:
-            os.makedirs('simulation-results/%d' % (nodeCnt) )
-        except OSError as e:
-            pass
-        
-        for iteration in range(iterations):
-            prefix = 'simulation-results/%d/%d-%d-' % (nodeCnt, nodeCnt, iteration)
-            print 'Generating %d-%d-scenario' % (nodeCnt, iteration)
-            generateConfig(1, nodeCnt-1, 0, prefix, 0)
-    
+        for it in s.iterations:
+            print 'Generating ', it.name()
+            if it.generator == 'simple':
+                generateConfig(1, it.nodeCnt-1, 0, it.prefixPath(), 0)
+            else:
+                raise Error('Unsupported generator: ' + it.generator)
     print 'Generating done'
 
 
-def finalizeProcess(p):
-    p.wait() 
-    os.remove(processInit[p])
-    os.remove(logProps[p])
-    stdoutFiles[p].flush()
-    stdoutFiles[p].close()
-    processes.remove(p)
+def finalizeSimulation(iteration):
+    iteration.simulation.wait() 
+    os.remove(iteration.omnetppPath())
+    os.remove(iteration.loggingPropertiesPath())
+    iteration.stdOut.flush()
+    iteration.stdOut.close()
+    simulated.remove(iteration)
 
 
-def simulateScenario(nodeCnt, iteration):
-    folder = root + '\simulation-results\%d\\' % (nodeCnt)        
-    prefix = '%d-%d-' % (nodeCnt, iteration)
+def simulateScenario(iteration):
+    #folder = root + '\simulation-results\%d\\' % (nodeCnt)        
+    #prefix = '%d-%d-' % (nodeCnt, iteration)
     classpath = root + '\..\dist\*;.'
     
-    logPropsFile = prefix + 'logging.properties'
-    logFile = folder + prefix + 'jdeeco.log'
-    logFile = logFile.replace('\\', '/')
+    #logPropsFile = prefix + 'logging.properties'
+    #logFile = folder + prefix + 'jdeeco.log'
+    #logFile = logFile.replace('\\', '/')
     
-    stdoutName = folder + prefix + 'stdout.log'
-    stdoutFile = open(stdoutName, 'w')
+    #stdoutName = folder + prefix + 'stdout.log'
     
-    copyfile(root + '\simulation-results\logging.properties', logPropsFile)
-    with open(logPropsFile , 'a') as f:
-        print>>f, '\n\njava.util.logging.FileHandler.pattern=' + logFile
     
-    #cmd = 'cd %s; java -cp %s cz.cuni.mff.d3s.jdeeco.simulation.demo.Main %s %s' % \
-    #    (root + folder, classpath, prefix + 'component.cfg', prefix + 'site.cfg')
+    copyfile(root + '\simulation-results\logging.properties', iteration.loggingPropertiesPath())
+    with open(iteration.loggingPropertiesPath() , 'a') as f:
+        print>>f, '\n\njava.util.logging.FileHandler.pattern=' + iteration.logTemplatePath().replace('\\', '/')
+   
     cmd = ['java', '-cp', classpath,
            '-Ddeeco.receive.cache.deadline="500"',
            '-Ddeeco.publish.individual="true"',
@@ -159,104 +154,102 @@ def simulateScenario(nodeCnt, iteration):
            '-Ddeeco.publish.packetsize="3000"',
            '-Ddeeco.publish.period="1000"',
            '-Ddeeco.rebroadcast.delay="1000"',
-           '-Djava.util.logging.config.file=%s' % (logPropsFile.replace('\\', '/')),
+           '-Djava.util.logging.config.file=%s' % (iteration.loggingPropertiesPath().replace('\\', '/')),
            'cz.cuni.mff.d3s.jdeeco.simulation.demo.Main',
-           folder + prefix + 'component.cfg', folder + prefix + 'site.cfg', prefix + 'omnetpp.ini' ]
+           iteration.componentCfgPath(), iteration.siteCfgPath(), iteration.omnetppPath() ]
     
-    if len(processes) >= cpus:
-        p = processes[0]
-        finalizeProcess(p)
+    if len(simulated) >= cpus:
+        finalizeSimulation(simulated[0])
     
-    print 'Evaluating', prefix + "scenario" 
+    print 'Evaluating', iteration.name() 
     print 'Executing: ', ' '.join(cmd)
-    p = Popen(cmd, stderr=STDOUT, stdout=stdoutFile)
     
-    processes.append(p)
-    processInit[p] = prefix + 'omnetpp.ini'
-    logProps[p] = logPropsFile
-    stdoutFiles[p] = stdoutFile
+    iteration.stdOut = open(iteration.stdOutPath(), 'w')
+    iteration.simulation = Popen(cmd, stderr=STDOUT, stdout=iteration.stdOut)
+     
+    simulated.append(iteration)
 
 
 def simulate():
     print 'Simulating...'
     
-    for nodeCnt in evaluations.keys():
-        for iteration in range(evaluations[nodeCnt]):
-            simulateScenario(nodeCnt, iteration)
+    for s in scenarios:       
+        for it in s.iterations:    
+            simulateScenario(it)
     
     # finalize the rest    
-    while len(processes) > 0:
-        finalizeProcess(processes[0])
+    while len(simulated) > 0:
+        finalizeSimulation(simulated[0])
     print 'Simulation done'
 
-demoAnalyses = {}
-genericAnalyses = {}
-neighborAnalyses = {}
-def analyzeScenario(nodeCnt, iteration):
-    folder = root + '\simulation-results\%d\\' % (nodeCnt)        
-    prefix = '%d-%d-' % (nodeCnt, iteration)
-    stdoutNameGeneric = folder + prefix + 'analysis-generic.txt'
-    stdoutNameDemo = folder + prefix + 'analysis-demo.txt'
-    logName =  folder + prefix + 'jdeeco.log.0'
-    componentConfigName = folder + prefix + 'component.cfg'
+
+def analyzeScenario(iteration):    
+    #folder = root + '\simulation-results\%d\\' % (nodeCnt)        
+    #prefix = '%d-%d-' % (nodeCnt, iteration)
+    #stdoutNameGeneric = folder + prefix + 'analysis-generic.txt'
+    #stdoutNameDemo = folder + prefix + 'analysis-demo.txt'
+    #logName =  folder + prefix + 'jdeeco.log.0'
+    #componentConfigName = folder + prefix + 'component.cfg'
    
-    print 'Analyzing', prefix + "scenario"
-  
-    with open(stdoutNameGeneric, 'w') as genericStdout:
+    print 'Analyzing', iteration.name()  
+    with open(iteration.genericAnalysisStdoutPath(), 'w') as genericStdout:
         oldStdOut = sys.stdout
         sys.stdout = genericStdout
         a = GenericAnalysis()
-        a.analyze(logName)        
+        a.analyze(iteration.logPath())        
         sys.stdout = oldStdOut
-        genericAnalyses[nodeCnt].append(a)       
+        iteration.genericAnalysis = a 
+        #genericAnalyses[nodeCnt].append(a)       
          
         #pGeneric = Popen(['python', root + '\\analysis\\analyze_log.py', logName], stderr=STDOUT, stdout=genericStdout)
         #pGeneric.wait()
     
-    with open(stdoutNameDemo, 'w') as demoStdout:
+    with open(iteration.demoAnalysisStdoutPath(), 'w') as demoStdout:
         oldStdOut = sys.stdout
         sys.stdout = demoStdout
         
         a = DemoAnalysis()
-        a.analyze(logName, componentConfigName)                   
+        a.analyze(iteration.logPath(), iteration.componentCfgPath())                   
         sys.stdout = oldStdOut
-        demoAnalyses[nodeCnt].append(a)
+        iteration.demoAnalysis = a 
+        #demoAnalyses[nodeCnt].append(a)
         #pDemo = Popen(['python', root + '\\analysis\\analyze_demo.py', logName, componentConfigName], stderr=STDOUT, stdout=demoStdout)
         #pDemo.wait()
 
 
     a = NeighborAnalysis()
-    a.analyze(componentConfigName)
-    neighborAnalyses[nodeCnt].append(a)
+    a.analyze(iteration.componentCfgPath())
+    iteration.neighborAnalysis = a
+    #neighborAnalyses[nodeCnt].append(a)
 
 def analyze():
     print 'Analyzing...'
     
-    runningAnalyses = []
-    
-    for nodeCnt in evaluations.keys():
-        demoAnalyses[nodeCnt] = []
-        genericAnalyses[nodeCnt] = []
-        neighborAnalyses[nodeCnt] = []
-        for iteration in range(evaluations[nodeCnt]):
-            analyzeScenario(nodeCnt, iteration)
-            
-        demoResultsFile = root + '\\simulation-results\\results-demo-%d.csv' % nodeCnt
-        with open(demoResultsFile, 'w') as results: 
-            for a in demoAnalyses[nodeCnt]:
+    for s in scenarios:    
+        for it in s.iterations:           
+            analyzeScenario(it)
+       
+        # demo analysis
+        with open(s.demoResultsPath(), 'w') as results: 
+            for it in s.iterations:
+                a = it.demoAnalysis
                 np.savetxt(results, zip(a.resTimes, a.resTimesNetwork, a.hops, a.versionDifs), fmt='%d')
-            demoAnalyses[nodeCnt] = []
-        genericResultsFile = root + '\\simulation-results\\results-generic-%d.csv' % nodeCnt
-        with open(genericResultsFile, 'w') as results: 
-            messageStats = [[a.sentMessagesCnt, a.receivedMessagesCnt] for a in genericAnalyses[nodeCnt]]           
+                it.demoAnalysis = None            
+        
+        # generic analysis
+        with open(s.genericResultsPath(), 'w') as results: 
+            messageStats = [[it.genericAnalysis.sentMessagesCnt, it.genericAnalysis.receivedMessagesCnt] for it in s.iterations]           
             np.savetxt(results, messageStats, fmt='%d')
-            genericAnalyses[nodeCnt] = []
-            
-        neighborsResultsFile = root + '\\simulation-results\\results-neighbors-%d.csv' % nodeCnt
-        with open(neighborsResultsFile, 'w') as results: 
-            neighbors = [cnt for a in neighborAnalyses[nodeCnt] for cnt in a.neighborCnts]            
+            for it in s.iterations:
+                it.genericAnalysis = None
+        
+        # neighbor analysis
+        with open(s.neighborResultsPath(), 'w') as results: 
+            neighbors = [cnt for it in s.iterations for cnt in it.neighborAnalysis.neighborCnts]            
             np.savetxt(results, neighbors, fmt='%d')
-            neighborAnalyses[nodeCnt] = []
+            for it in s.iterations:
+                it.neighborAnalysis = None
+                
     print 'Analysis done'
 
 def colorBoxplot(bp):
@@ -265,51 +258,45 @@ def colorBoxplot(bp):
     pylab.setp(bp['fliers'], marker='None')
 def plot():    
     print 'Plotting...'
-    results = {}
-    neighbors = {}
-    messages = {}
+    
     
     pylab.hold(True)
    
-    
-    for nodeCnt in evaluations.keys():       
-        results[nodeCnt] = []
-        neighbors[nodeCnt] = []
-        messages[nodeCnt] = []
-        
-        with open(root + '\\simulation-results\\results-demo-%d.csv' % nodeCnt , 'r') as resultsFile: 
+    for s in scenarios:        
+        with open(s.demoResultsPath() , 'r') as resultsFile: 
             contents = np.loadtxt(resultsFile)
-            results[nodeCnt] = map(int, contents[:, 1])
-        with open(root + '\\simulation-results\\results-generic-%d.csv' % nodeCnt , 'r') as resultsFile: 
+            s.node2nodeResponseTimes = map(int, contents[:, 1])
+        with open(s.genericResultsPath(), 'r') as resultsFile: 
             contents = np.loadtxt(resultsFile)
             sent = map(int, contents[:, 0])            
             received = map(int, contents[:, 1])
-            messages[nodeCnt] = [average(sent), average(received), average(received)*1.0/average(sent)]
-        with open(root + '\\simulation-results\\results-neighbors-%d.csv' % nodeCnt , 'r') as resultsFile: 
+            s.messageStats = [average(sent), average(received), average(received)*1.0/average(sent)]
+        with open(s.neighborResultsPath() , 'r') as resultsFile: 
             contents = np.loadtxt(resultsFile)
-            neighbors[nodeCnt] = map(int, contents)
+            s.neighbors = map(int, contents)
     
         pylab.figure(0)
-        bp = pylab.boxplot(results[nodeCnt], positions = [nodeCnt], widths = 2)
+        bp = pylab.boxplot(s.node2nodeResponseTimes, positions = [s.nodeCnt], widths = 2)        
         colorBoxplot(bp)
         pylab.figure(1)
-        bp = pylab.boxplot(neighbors[nodeCnt], positions = [nodeCnt], widths = 2)
+        bp = pylab.boxplot(s.neighbors, positions = [s.nodeCnt], widths = 2)
         colorBoxplot(bp)
     
     pylab.figure(0)
-    pylab.title('End-to-end response')
+    pylab.title('End-to-end response')    
     pylab.axes().set_yticks(range(0, 60000, 5000))
-    pylab.axes().set_yticklabels(range(0, 60, 5))        
-    
+    pylab.axes().set_yticklabels(range(0, 60, 5))    
     pylab.figure(1)
     pylab.title('Number of neighbors')
     
     
+    nodeTicks = [s.plotTick() for s in scenarios]
+    nodeCounts = [s.nodeCnt for s in scenarios]
     for fig in range(2):
-        pylab.figure(fig)     
-        ax = pylab.axes()
-        ax.set_xticks(evaluations.keys())       
-        pylab.xlim(min(evaluations.keys())-3,max(evaluations.keys()) +3)
+        pylab.figure(fig)      
+        pylab.axes().set_xticks(nodeCounts)       
+        pylab.axes().set_xticklabels(nodeTicks)
+        pylab.xlim(min(nodeCounts)-3,max(nodeCounts) +3)
         
         
     pylab.show()
