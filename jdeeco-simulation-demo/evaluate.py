ca66a1d047152d609c6d23033608c32e3286ea8d
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2014-02-17 15:23:53 +0100

    further analysis

--- jdeeco-simulation-demo/evaluate.py
+++ jdeeco-simulation-demo/evaluate.py
@@ -1,6 +1,8 @@
 import os, sys
 from generator.simple import generateConfig
 from analysis.analyze_demo import *
+from analysis.analyze_log import *
+from analysis.analyze_neighbors import *
 from exceptions import OSError
 from subprocess import *
 import atexit
@@ -9,6 +11,7 @@ import time
 from shutil import copyfile
 import numpy as np
 import pylab
+from numpy import average
 
 processes = []
 processInit = {}
@@ -33,7 +36,7 @@ def cleanup():
 atexit.register(cleanup)
 root = os.path.dirname(os.path.realpath(__file__))
 #evaluations = {4:10, 8:10, 12: 10, 16:10, 20:10}
-evaluations = {8:10, 12: 10, 16:10, 20:10, 30:10, 40:10, 50:10, 60:10}
+evaluations = {8:10, 12: 10, 16:10, 20:10, 24:10, 28:10}
 
 def generate():
     print 'Generating configurations...'
@@ -47,7 +50,7 @@ def generate():
         for iteration in range(iterations):
             prefix = 'simulation-results/%d/%d-%d-' % (nodeCnt, nodeCnt, iteration)
             print 'Generating %d-%d-scenario' % (nodeCnt, iteration)
-            generateConfig(1, nodeCnt-1, 0, prefix)
+            generateConfig(1, nodeCnt-1, 0, prefix, 0)
     
     print 'Generating done'
 
@@ -82,7 +85,7 @@ def simulateScenario(nodeCnt, iteration):
     cmd = ['java', '-cp', classpath,
            '-Ddeeco.receive.cache.deadline="500"',
            '-Ddeeco.publish.individual="true"',
-           '-Ddeeco.boundary.disable="true"',
+           '-Ddeeco.boundary.disable="false"',
            '-Ddeeco.publish.packetsize="3000"',
            '-Ddeeco.publish.period="1000"',
            '-Ddeeco.rebroadcast.delay="1000"',
@@ -117,6 +120,8 @@ def simulate():
     print 'Simulation done'
 
 demoAnalyses = {}
+genericAnalyses = {}
+neighborAnalyses = {}
 def analyzeScenario(nodeCnt, iteration):
     folder = root + '\simulation-results\%d\\' % (nodeCnt)        
     prefix = '%d-%d-' % (nodeCnt, iteration)
@@ -127,67 +132,115 @@ def analyzeScenario(nodeCnt, iteration):
    
     print 'Analyzing', prefix + "scenario"
   
-    genericStdout = open(stdoutNameGeneric, 'w')
-    pGeneric = Popen(['python', root + '\\analysis\\analyze_log.py', logName], stderr=STDOUT, stdout=genericStdout)
-   
-    demoStdout = open(stdoutNameDemo, 'w')
-    oldStdOut = sys.stdout
-    sys.stdout = demoStdout
-    #pDemo = Popen(['python', root + '\\analysis\\analyze_demo.py', logName, componentConfigName], stderr=STDOUT, stdout=demoStdout)
-    a = DemoAnalysis()
-    a.analyze(logName, componentConfigName)
-    if not nodeCnt in demoAnalyses:
-        demoAnalyses[nodeCnt] = []
-    demoAnalyses[nodeCnt].append(a)
-    sys.stdout = oldStdOut
+    with open(stdoutNameGeneric, 'w') as genericStdout:
+        oldStdOut = sys.stdout
+        sys.stdout = genericStdout
+        a = GenericAnalysis()
+        a.analyze(logName)        
+        sys.stdout = oldStdOut
+        genericAnalyses[nodeCnt].append(a)       
+         
+        #pGeneric = Popen(['python', root + '\\analysis\\analyze_log.py', logName], stderr=STDOUT, stdout=genericStdout)
+        #pGeneric.wait()
     
-    pGeneric.wait()
-    genericStdout.close()
-    demoStdout.close()
-    demoAnalyses.clear()
+    with open(stdoutNameDemo, 'w') as demoStdout:
+        oldStdOut = sys.stdout
+        sys.stdout = demoStdout
+        
+        a = DemoAnalysis()
+        a.analyze(logName, componentConfigName)                   
+        sys.stdout = oldStdOut
+        demoAnalyses[nodeCnt].append(a)
+        #pDemo = Popen(['python', root + '\\analysis\\analyze_demo.py', logName, componentConfigName], stderr=STDOUT, stdout=demoStdout)
+        #pDemo.wait()
 
+
+    a = NeighborAnalysis()
+    a.analyze(componentConfigName)
+    neighborAnalyses[nodeCnt].append(a)
 
 def analyze():
     print 'Analyzing...'
+    
+    runningAnalyses = []
+    
     for nodeCnt in evaluations.keys():
+        demoAnalyses[nodeCnt] = []
+        genericAnalyses[nodeCnt] = []
+        neighborAnalyses[nodeCnt] = []
         for iteration in range(evaluations[nodeCnt]):
             analyzeScenario(nodeCnt, iteration)
-        resultsFile = root + '\\simulation-results\\results-%d.csv' % nodeCnt
-        with open(resultsFile, 'w') as results: 
+            
+        demoResultsFile = root + '\\simulation-results\\results-demo-%d.csv' % nodeCnt
+        with open(demoResultsFile, 'w') as results: 
             for a in demoAnalyses[nodeCnt]:
                 np.savetxt(results, zip(a.resTimes, a.resTimesNetwork, a.hops, a.versionDifs), fmt='%d')
+            demoAnalyses[nodeCnt] = []
+        genericResultsFile = root + '\\simulation-results\\results-generic-%d.csv' % nodeCnt
+        with open(genericResultsFile, 'w') as results: 
+            messageStats = [[a.sentMessagesCnt, a.receivedMessagesCnt] for a in genericAnalyses[nodeCnt]]           
+            np.savetxt(results, messageStats, fmt='%d')
+            genericAnalyses[nodeCnt] = []
+            
+        neighborsResultsFile = root + '\\simulation-results\\results-neighbors-%d.csv' % nodeCnt
+        with open(neighborsResultsFile, 'w') as results: 
+            neighbors = [cnt for a in neighborAnalyses[nodeCnt] for cnt in a.neighborCnts]            
+            np.savetxt(results, neighbors, fmt='%d')
+            neighborAnalyses[nodeCnt] = []
     print 'Analysis done'
+
+def colorBoxplot(bp):
+    pylab.setp(bp['boxes'], color='black')
+    pylab.setp(bp['whiskers'], color='black')
 
 def plot():    
     print 'Plotting...'
     results = {}
+    neighbors = {}
+    messages = {}
     
     pylab.hold(True)
     ax = pylab.axes()
     
-    for nodeCnt in evaluations.keys():     
-        resultsFileName = root + '\\simulation-results\\results-%d.csv' % nodeCnt        
+    for nodeCnt in evaluations.keys():       
         results[nodeCnt] = []
+        neighbors[nodeCnt] = []
+        messages[nodeCnt] = []
         
-        with open(resultsFileName, 'r') as resultsFile: 
+        with open(root + '\\simulation-results\\results-demo-%d.csv' % nodeCnt , 'r') as resultsFile: 
             contents = np.loadtxt(resultsFile)
             results[nodeCnt] = map(int, contents[:, 1])
+        with open(root + '\\simulation-results\\results-generic-%d.csv' % nodeCnt , 'r') as resultsFile: 
+            contents = np.loadtxt(resultsFile)
+            sent = map(int, contents[:, 0])            
+            received = map(int, contents[:, 1])
+            messages[nodeCnt] = [average(sent), average(received), average(received)*1.0/average(sent)]
+        with open(root + '\\simulation-results\\results-neighbors-%d.csv' % nodeCnt , 'r') as resultsFile: 
+            contents = np.loadtxt(resultsFile)
+            neighbors[nodeCnt] = map(int, contents)
     
+        pylab.figure(0)
         bp = pylab.boxplot(results[nodeCnt], positions = [nodeCnt], widths = 2)
-    ax.set_xticks(evaluations.keys())
+        colorBoxplot(bp)
+        pylab.figure(1)
+        bp = pylab.boxplot(neighbors[nodeCnt], positions = [nodeCnt], widths = 2)
+        colorBoxplot(bp)
+    
+    pylab.figure(0)
+    ax.set_xticks(evaluations.keys())       
     pylab.xlim(min(evaluations.keys())-3,max(evaluations.keys()) +3)
     pylab.savefig("results.png")
+    
     pylab.show()
+    
     print 'Plotting done'
- #   pylab.setp(bp['boxes'], color='black')
-  #  pylab.setp(bp['whiskers'], color='black')
-   # pylab.setp(bp['fliers'], marker='None')    
-    #pylab.show()
+     
+    
     
     
     
 if __name__ == '__main__':
-    generate()
-    simulate()
-    analyze()
+    #generate()
+    #simulate()
+    #analyze()
     plot()
