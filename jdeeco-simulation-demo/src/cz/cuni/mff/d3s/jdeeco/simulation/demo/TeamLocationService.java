d7f37c9641ffc18fd096d79bb4e165d4e5b6de36
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2014-04-03 10:28:21 +0200

    boundary condition updated to allow FFs in the outside world

--- jdeeco-simulation-demo/src/cz/cuni/mff/d3s/jdeeco/simulation/demo/TeamLocationService.java
+++ jdeeco-simulation-demo/src/cz/cuni/mff/d3s/jdeeco/simulation/demo/TeamLocationService.java
@@ -1,18 +1,20 @@
 package cz.cuni.mff.d3s.jdeeco.simulation.demo;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import cz.cuni.mff.d3s.deeco.logging.Log;
+
 public enum TeamLocationService {
 	INSTANCE;
 	
 	private final Map<String, Set<Area>> teamSites = new HashMap<>();
-	
+	private final Set<Area> sites = new HashSet<>();
 	
 	public void init(Set<Area> sites) {	
+		this.sites.addAll(sites);
 		for(Area a: sites) {
 			for (String team: a.getTeams()) {
 				if (!teamSites.containsKey(team))
@@ -23,13 +25,20 @@ public enum TeamLocationService {
 	}
 	
 	public boolean isAtTheTeamsSite(String teamId, Position pos) {
-		try {
-			for (Area site: teamSites.get(teamId)) {		
+		// The team has no registered site - it is in the "outside world"
+		if (!teamSites.containsKey(teamId)) {
+			// then return true if the position is outside every registered area
+			for (Area site: sites) {
 				if (site.isInArea(pos))
-					return true;
+					return false;
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			return true;
+		}
+		// else check whether the position belongs to some of the registered
+		// areas for the team 
+		for (Area site: teamSites.get(teamId)) {		
+			if (site.isInArea(pos))
+				return true;
 		}
 		return false;
 	}
