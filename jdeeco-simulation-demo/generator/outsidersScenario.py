26374c6e055eb07a11883f0d4cccd17b43a99b95
Michał Kit <kit@d3s.mff.cuni.cz>
2014-04-08 11:28:07 +0200

    New scenarios

--- jdeeco-simulation-demo/generator/outsidersScenario.py
+++ jdeeco-simulation-demo/generator/outsidersScenario.py
@@ -13,7 +13,103 @@ def memberToLeader(member):
     leader.ip = member.ip
     return leader
 
-def generateCrossAreas(density, cellSize, thickness, xSize, ySize, radioDistance, leaderNumber, ipCount, prefix):
+def overlapingAreas(density, cellSize, areaCount, areaSize, overlap, radioDistance, leaderNumber, ipCountPerTeam, prefix):
+    scale = 250 / radioDistance
+    
+    cellCountX = areaCount*areaSize - overlap*(areaCount-1)
+    sizeX = cellCountX*cellSize*scale
+    sizeY = scale*cellSize*areaSize
+    fig = figure()
+    plot = fig.add_subplot(111, aspect='equal')
+    plot.set_xlim(0, sizeX)
+    plot.set_ylim(0, sizeY)
+    
+    areas = []
+    teams = []
+    for i in range(areaCount):
+        area = RectanguralArea('A'+str(i), i*(areaSize-overlap)*cellSize, 0, areaSize*cellSize, areaSize*cellSize, [i, i+1])
+        area.scale(scale)
+        plot.add_artist(area.getPlotObject(color='k'))
+        areas.append(area)
+        teams.append([])
+    
+    totalDensity = density // 1
+    totalDensity = int(totalDensity)
+    
+    #Generate members
+    m = []
+    if totalDensity > 0:
+        for i in range(cellCountX):
+            for j in range(areaSize):
+                for d in range(totalDensity):
+                    point = randomPoint(cellSize*i, cellSize*j, cellSize, cellSize)
+                    component = Member(-1, point[0]*scale, point[1]*scale);
+                    m.append(component)
+            
+    fracDensity = density - totalDensity
+    if fracDensity > 0:
+        toAdd = random.sample(range(cellCountX*areaSize), int(cellCountX*areaSize*fracDensity))
+        for i in range(cellCountX):
+            for j in range(areaSize):
+                if (j*cellCountX + i) in toAdd:
+                    point = randomPoint(cellSize*i, cellSize*j, cellSize, cellSize)
+                    component = Member(-1, point[0]*scale, point[1]*scale);
+                    m.append(component)
+    #Assign teams               
+    for ti in range(len(teams)):
+        area = areas[ti]
+        for member in m:
+            if member.x >= area.x and member.x <= area.x + area.width:
+                if member.team > -1:
+                    probability = random.random()
+                else:
+                    probability = 1
+                if probability > 0.5:
+                    member.team = ti
+                teams[ti].append(member)
+                
+                
+    #Assign leaders
+    toLeader = random.sample(teams[0], leaderNumber)
+    leaders = []
+    for l in toLeader:
+        m.remove(l)
+        teams[0].remove(l)
+        leader = memberToLeader(l)
+        teams[0].append(leader)
+        leaders.append(leader)
+        
+    #IP support
+    for team in teams:
+        ips = random.sample(team, int(len(team)*ipCountPerTeam))
+        for i in ips:
+            i.ip = True 
+    
+    f = open(prefix + 'component.cfg', 'w') 
+
+    for i in range(len(leaders)):
+        leaders[i].plot(plot, 'L'+str(i))
+        print>>f, leaders[i].toString(i)
+       
+    for i in range(len(m)):
+        m[i].plot(plot, 'M'+str(i))  
+        print>>f, m[i].toString(i)
+               
+    f.close()
+    
+    f = open(prefix + 'site.cfg', 'w') 
+    print>>f, sizeX, sizeY
+    for a in areas:
+        print>>f, a.toString();
+    f.close()    
+    
+    
+    savefig(prefix + "cfg.png")
+    if __name__ == '__main__':
+        show()
+    close()
+
+def crossAreas(density, cellSize, thickness, xSize, ySize, radioDistance, leaderNumber, ipCount, prefix):
     scale = 250 / radioDistance
     
     sizeX = scale*cellSize*xSize
@@ -52,8 +148,8 @@ def generateCrossAreas(density, cellSize, thickness, xSize, ySize, radioDistance
                             component.team = 1
                             v.append(component)
                         
+   
     fracDensity = density - totalDensity
-    
     totalInAreaCellCount = (xSize + ySize - thickness)*thickness
     
     if fracDensity > 0:
@@ -120,7 +216,7 @@ def generateCrossAreas(density, cellSize, thickness, xSize, ySize, radioDistance
     print len(all)
     
 
-def generate2AreasPlayground(density, cellSize,  areaSizeX, areaSizeY, margin, radioDistance, leadersDistribution, ipCount, prefix):
+def twoAreasPlayground(density, cellSize,  areaSizeX, areaSizeY, margin, radioDistance, leadersDistribution, ipCount, prefix):
     scale = 250 / radioDistance # scale the scenario to match the required radio distance
     
     totalCellCountX = 3*margin + 2*areaSizeX  
@@ -243,4 +339,6 @@ def generate2AreasPlayground(density, cellSize,  areaSizeX, areaSizeY, margin, r
     (density, cellSize,  areaSizeX, areaSizeY, margin, radioDistance, leadersDistribution, ipCount, prefix)
 if __name__ == '__main__':
     #generate2AreasPlayground(1.5, 20, 4, 4, 2, 25, [2,2,0], [0.2, 0.2, 0.2], '')
-    generateCrossAreas(1, 20, 4, 10, 10, 25, 2, 0.25, '')
+    #generateCrossAreas(1, 20, 4, 10, 10, 25, 2, 0.25, '')
+    #(density, cellSize, areaCount, areaSize, overlap, radioDistance, leaderNumber, ipCountPerTeam, prefix)
+    generateOverlapingAreas(1, 20, 4, 6, 2, 25, 2, 0.25, '')
