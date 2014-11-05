07e79c595b1b4f69774f9452e5de39d9cef5396a
Vladimir Matena <matena@d3s.mff.cuni.cz>
2014-11-05 16:19:20 +0100

    Apply partitioned by annotation on the ensemble class

--- jdeeco-core/src/cz/cuni/mff/d3s/deeco/annotations/PartitionedBy.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/deeco/annotations/PartitionedBy.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  * 
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 public @interface PartitionedBy {
 	String value();
 }
