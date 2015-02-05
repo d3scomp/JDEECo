861a79e2f4673ae0830d61f2893ec40387d7f8fe
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-05 14:23:55 +0100

    Add dummy implementations of marshalling

--- jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/network/marshaller/MarshallerRegistry.java
+++ jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/network/marshaller/MarshallerRegistry.java
@@ -1,5 +1,21 @@
 package cz.cuni.mff.d3s.jdeeco.network.marshaller;
 
-public class MarshallerRegistry {
+import cz.cuni.mff.d3s.jdeeco.network.PacketType;
 
+public class MarshallerRegistry {
+	byte[] marshall(PacketType type, Object data) {
+		return null;
+	}
+	
+	Object unmarshall(int type, byte[] data) {
+		return null;
+	}
+	
+	PacketType resolvePacketType(int type) {
+		throw new RuntimeException();
+	}
+	
+	void registerMarshaller(PacketType type, Marshaller marshaller) {
+		throw new RuntimeException();
+	}
 }
---
+++ jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/network/marshaller/SerializingMarshaller.java
@@ -0,0 +1,11 @@
+package cz.cuni.mff.d3s.jdeeco.network.marshaller;
+
+public class SerializingMarshaller implements Marshaller {
+	public byte[] marshall(Object data) {
+		return null;
+	}
+
+	public Object unmashall(byte[] data) {
+		return null;
+	}
+}
