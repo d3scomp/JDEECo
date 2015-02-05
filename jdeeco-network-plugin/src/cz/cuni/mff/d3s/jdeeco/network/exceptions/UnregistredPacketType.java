f40bde6d4e846f73c5822fb5ea5b43faff114fe6
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-05 14:04:14 +0100

    Add exception for unregistered packet type

---
+++ jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/network/exceptions/UnregistredPacketType.java
@@ -0,0 +1,14 @@
+package cz.cuni.mff.d3s.jdeeco.network.exceptions;
+
+public class UnregistredPacketType extends Exception {
+	private int type;
+	
+	protected UnregistredPacketType(int type) {
+		this.type = type;
+	}
+	
+	@Override
+	public String getMessage() {
+		return String.format("Packet type %d is has no registred for marschalling.", type);
+	}
+}
