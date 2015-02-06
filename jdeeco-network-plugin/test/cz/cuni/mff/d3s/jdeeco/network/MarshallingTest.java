829e1da39c70f0851e3a2f4cf411a9ed9297cd30
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-06 12:34:07 +0100

    Add complex payload test for marshalling

--- jdeeco-network-plugin/test/cz/cuni/mff/d3s/jdeeco/network/MarshallingTest.java
+++ jdeeco-network-plugin/test/cz/cuni/mff/d3s/jdeeco/network/MarshallingTest.java
@@ -2,8 +2,10 @@ package cz.cuni.mff.d3s.jdeeco.network;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.*;
+
+import org.junit.*;
+import org.omg.CORBA.COMM_FAILURE;
 
 import cz.cuni.mff.d3s.jdeeco.network.exceptions.UnregistredPacketType;
 import cz.cuni.mff.d3s.jdeeco.network.marshaller.MarshallerRegistry;
@@ -22,6 +24,30 @@ public class MarshallingTest {
 	// Test pay-load for marshaling
 	private final String SIMPLE_PAYLOAD = "payload";
 
+	// Complex test pay-load for marshaling
+	private final Map<Integer, String> COMPLEX_PAYLOAD = new HashMap<Integer, String>();
+
+	public MarshallingTest() {
+		COMPLEX_PAYLOAD.put(1, "One");
+		COMPLEX_PAYLOAD.put(2, "Two");
+		COMPLEX_PAYLOAD.put(3, "Three");
+		COMPLEX_PAYLOAD.put(4, "Four");
+		COMPLEX_PAYLOAD.put(5, "Five");
+	}
+
+	private void assertSimplePayload(Object payload) {
+		assertEquals(SIMPLE_PAYLOAD, payload);
+	}
+
+	private void assertComplexPayload(Object payload) {
+		@SuppressWarnings("unchecked")
+		Map<Integer, String> payloadMap = (Map<Integer, String>) payload;
+
+		assertEquals(COMPLEX_PAYLOAD.size(), payloadMap.size());
+		assertTrue("Checks whenever the COMPLEX_PAYLOAD (hashmap) contains all entries", COMPLEX_PAYLOAD.entrySet()
+				.containsAll(payloadMap.entrySet()));
+	}
+
 	/**
 	 * Initializes registry and registers knowledge marshaler
 	 */
@@ -36,14 +62,26 @@ public class MarshallingTest {
 	}
 
 	/**
-	 * Tests encode and decode of payload using knowledge marshaler
+	 * Tests encode and decode of pay-load using knowledge marshaler
 	 * 
 	 * @throws UnregistredPacketType
 	 */
 	@Test
-	public void testEncodeDecodeByValue() throws UnregistredPacketType {
+	public void testEncodeDecodeSimple() throws UnregistredPacketType {
 		byte[] data = registry.marshall(PacketTypes.KNOWLEDGE, SIMPLE_PAYLOAD);
 		Object obj = registry.unmarshall(PacketTypes.KNOWLEDGE, data);
-		assertEquals(SIMPLE_PAYLOAD, obj);
+		assertSimplePayload(obj);
+	}
+	
+	/**
+	 * Tests encode and decode of complex pay-load using knowledge marshaler
+	 * 
+	 * @throws UnregistredPacketType
+	 */
+	@Test
+	public void testEncodeDecodeComplex() throws UnregistredPacketType {
+		byte[] data = registry.marshall(PacketTypes.KNOWLEDGE, COMPLEX_PAYLOAD);
+		Object obj = registry.unmarshall(PacketTypes.KNOWLEDGE, data);
+		assertComplexPayload(obj);
 	}
 }
