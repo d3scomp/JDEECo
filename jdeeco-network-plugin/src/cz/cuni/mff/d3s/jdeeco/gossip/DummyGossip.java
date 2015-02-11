0369e52bd87cd26436f8d3a127b705f5baa925e5
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-11 16:26:25 +0100

    Pass data to network in dummy gossip implementation

--- jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/gossip/DummyGossip.java
+++ jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/gossip/DummyGossip.java
@@ -1,26 +1,41 @@
 package cz.cuni.mff.d3s.jdeeco.gossip;
 
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 
 import cz.cuni.mff.d3s.deeco.runtime.DEECoContainer;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoPlugin;
+import cz.cuni.mff.d3s.jdeeco.network.Network;
+import cz.cuni.mff.d3s.jdeeco.network.NetworkPlugin;
 
+/**
+ * Trivial gossip layer implementation, just passes data to network
+ * 
+ * @author Vladimir Matena <matena@d3s.mff.cuni.cz>
+ *
+ */
 public class DummyGossip implements Gossip {
+	private NetworkPlugin network;
+
 	@Override
 	public List<Class<? extends DEECoPlugin>> getDependencies() {
-		// No dependencies so far
-		return new LinkedList<Class<? extends DEECoPlugin>>();
+		return Arrays.asList(Network.class);
 	}
 
 	@Override
 	public void init(DEECoContainer container) {
+		// Resolve network layer dependency
+		network = container.getPluginInstance(Network.class);
+
 		// TODO Register for knowledge updates
+
+		// container.getRuntimeFramework()
 
 	}
 
 	@Override
 	public void processKnowledgeUpdate(Object knowledge) {
-		throw new UnsupportedOperationException();
+		// TODO: We should distinguish type as this is the last place where the type is known
+		network.processDataFromGossipLayer(knowledge);
 	}
 }
