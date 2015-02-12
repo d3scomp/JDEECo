6a4c6ce4835f72c12bd4013df0c2d67aa4a07cba
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-12 13:48:42 +0100

    Add Layer 2 package info

---
+++ jdeeco-network-plugin/src/cz/cuni/mff/d3s/jdeeco/network/l2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * jDEECo Network Layer 2
+ * 
+ * Layer 2 network is intended to process assembled knowledge (or other data) fragments by strategies. Layer 2
+ * strategies are expected to pass the data to upper layers and judge about re-broadcasting the data back to
+ * lower layers. 
+ */
+package cz.cuni.mff.d3s.jdeeco.network.l2;
