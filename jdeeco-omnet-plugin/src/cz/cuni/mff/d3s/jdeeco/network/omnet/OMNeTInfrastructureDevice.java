5c34d7979dfb26555c3930cd6b15229bd9677983
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-02-13 17:36:10 +0100

    Add prototypes of omnet interfaces

---
+++ jdeeco-omnet-plugin/src/cz/cuni/mff/d3s/jdeeco/network/omnet/OMNeTInfrastructureDevice.java
@@ -0,0 +1,36 @@
+package cz.cuni.mff.d3s.jdeeco.network.omnet;
+
+import cz.cuni.mff.d3s.deeco.runtime.DEECoContainer;
+import cz.cuni.mff.d3s.jdeeco.network.address.Address;
+import cz.cuni.mff.d3s.jdeeco.network.address.IPAddress;
+
+public class OMNeTInfrastructureDevice extends OMNeTDevice {
+	public OMNeTInfrastructureDevice(OMNeTSimulation simulation /* , address, virtual lan, ... */) {
+		super(simulation);
+	}
+
+	@Override
+	public String getId() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int getMTU() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean canSend(Address address) {
+		return address instanceof IPAddress;
+	}
+
+	@Override
+	public void send(byte[] data, Address address) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void init(DEECoContainer container) {
+		throw new UnsupportedOperationException();
+	}
+}
--- jdeeco-omnet-plugin/src/cz/cuni/mff/d3s/jdeeco/network/omnet/OMNeTSimulation.java
+++ jdeeco-omnet-plugin/src/cz/cuni/mff/d3s/jdeeco/network/omnet/OMNeTSimulation.java
@@ -1,12 +1,7 @@
 package cz.cuni.mff.d3s.jdeeco.network.omnet;
 
-import java.util.List;
 
-import cz.cuni.mff.d3s.deeco.runtime.DEECoContainer;
-import cz.cuni.mff.d3s.deeco.runtime.DEECoPlugin;
-import cz.cuni.mff.d3s.jdeeco.network.Network;
-
-public class OMNeTSimulation implements Notifier, DEECoPlugin {
+public class OMNeTSimulation implements Notifier {
 
 	public OMNeTSimulation() {
 		// TODO Auto-generated constructor stub
@@ -22,19 +17,4 @@ public class OMNeTSimulation implements Notifier, DEECoPlugin {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
-	@Override
-	public List<Class<? extends DEECoPlugin>> getDependencies() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void init(DEECoContainer container) {
-		// TODO Auto-generated method stub
-		
-		container.getPluginInstance(Network.class).getL1().registerDevice(device);
-
-	}
-
 }
