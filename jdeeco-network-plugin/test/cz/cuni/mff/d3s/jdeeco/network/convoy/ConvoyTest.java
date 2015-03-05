193548f50330d85150a6d4dfff7a2993453a886d
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-03-05 12:49:41 +0100

    Update networked convoy integration test

--- jdeeco-network-plugin/test/cz/cuni/mff/d3s/jdeeco/network/convoy/ConvoyTest.java
+++ jdeeco-network-plugin/test/cz/cuni/mff/d3s/jdeeco/network/convoy/ConvoyTest.java
@@ -1,12 +1,18 @@
 package cz.cuni.mff.d3s.jdeeco.network.convoy;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 
 import cz.cuni.mff.d3s.deeco.annotations.processor.AnnotationProcessorException;
+import cz.cuni.mff.d3s.deeco.runners.DEECoSimulation;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoException;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoNode;
 import cz.cuni.mff.d3s.deeco.timer.DiscreteEventTimer;
+import cz.cuni.mff.d3s.deeco.timer.SimulationTimer;
 import cz.cuni.mff.d3s.jdeeco.network.Network;
 import cz.cuni.mff.d3s.jdeeco.network.device.LoopbackBroadcastDevice;
 import cz.cuni.mff.d3s.jdeeco.network.l2.strategy.KnowledgeInsertingStrategy;
@@ -18,35 +24,46 @@ import cz.cuni.mff.d3s.jdeeco.publishing.DummyKnowledgePublisher;
  *
  */
 public class ConvoyTest {	
+	@Rule
+	public final StandardOutputStreamLog  log = new StandardOutputStreamLog ();
+	
 	public static void main(String[] args) throws AnnotationProcessorException, InterruptedException, DEECoException {
 		ConvoyTest test = new ConvoyTest();
 
 		test.testConvoyLoopback();
 	}
 
-	// #######################################
-	//  LOOPBACK NETWORKING
-	// #######################################
-	@Test
+	/**
+	 * Test network with broadcasting loop- back device
+	 * 
+	 * @throws AnnotationProcessorException
+	 * @throws InterruptedException
+	 * @throws DEECoException
+	 */
+	//@Test
 	public void testConvoyLoopback() throws AnnotationProcessorException, InterruptedException, DEECoException {
-		DiscreteEventTimer timer = new DiscreteEventTimer();
-		
-		LoopbackBroadcastDevice loopback = new LoopbackBroadcastDevice();
-		
-		// Create main application container
-		DEECoNode deeco1 = new DEECoNode(timer, new Network(), loopback,
-				new DummyKnowledgePublisher(), new KnowledgeInsertingStrategy());
-
-		DEECoNode deeco2 = new DEECoNode(timer, new Network(), loopback,
-				new DummyKnowledgePublisher(), new KnowledgeInsertingStrategy());
-
-		// Deploy components and ensembles
+		/* create main application container */
+		SimulationTimer simulationTimer = new DiscreteEventTimer(); // also "new WallTimeSchedulerNotifier()" 
+ 		DEECoSimulation realm = new DEECoSimulation(simulationTimer);
+ 		
+ 		LoopbackBroadcastDevice loopback = new LoopbackBroadcastDevice();
+		 
+		/* create first deeco node */
+		DEECoNode deeco1 = realm.createNode(new Network(), new DummyKnowledgePublisher(), new KnowledgeInsertingStrategy(), loopback);
+		/* deploy components and ensembles */
 		deeco1.deployComponent(new Leader());
 		deeco1.deployEnsemble(ConvoyEnsemble.class);
-
+		
+		/* create second deeco node */
+		DEECoNode deeco2 = realm.createNode(new Network(), new DummyKnowledgePublisher(), new KnowledgeInsertingStrategy(), loopback);
+		/* deploy components and ensembles */
 		deeco2.deployComponent(new Follower());
 		deeco2.deployEnsemble(ConvoyEnsemble.class);
 
-		timer.start(600000);
+		/* WHEN simulation is performed */
+		realm.start(20000);
+
+		// THEN the follower prints out the following (as there is no network and the components cannot exchange data)
+		assertThat(log.getLog(), containsString("Follower F: me = (1,1) leader = null"));
 	}
 }
