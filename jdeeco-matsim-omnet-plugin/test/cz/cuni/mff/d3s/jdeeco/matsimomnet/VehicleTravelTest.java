0125844acef356f58046928f8a678244fd53e074
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-04-21 18:02:44 +0200

    Update matsim omnet test to really test the expected behavior

--- jdeeco-matsim-omnet-plugin/test/cz/cuni/mff/d3s/jdeeco/matsimomnet/VehicleTravelTest.java
+++ jdeeco-matsim-omnet-plugin/test/cz/cuni/mff/d3s/jdeeco/matsimomnet/VehicleTravelTest.java
@@ -18,6 +18,7 @@ import cz.cuni.mff.d3s.deeco.runtime.DEECoNode;
 import cz.cuni.mff.d3s.jdeeco.matsim.plugin.MATSimVehicle;
 import cz.cuni.mff.d3s.jdeeco.network.Network;
 import cz.cuni.mff.d3s.jdeeco.network.l2.strategy.KnowledgeInsertingStrategy;
+import cz.cuni.mff.d3s.jdeeco.network.omnet.OMNeTBroadcastDevice;
 import cz.cuni.mff.d3s.jdeeco.network.omnet.OMNeTSimulation;
 import cz.cuni.mff.d3s.jdeeco.position.PositionAware;
 import cz.cuni.mff.d3s.jdeeco.publishing.DefaultKnowledgePublisher;
@@ -34,11 +35,11 @@ public class VehicleTravelTest {
 
 	public static void main(String[] args) throws AnnotationProcessorException, InterruptedException, DEECoException,
 			InstantiationException, IllegalAccessException, IOException {
-		new VehicleTravelTest().testTravel();
+		new VehicleTravelTest().testTravelMobilityLimitedRangeOmnet();
 	}
 
-	@Test @Ignore("We currently cannot run multiple OMNeT based simulations AND this is not yet ready to work properly")
-	public void testTravel() throws AnnotationProcessorException, InterruptedException, DEECoException,
+	@Test //@Ignore("We currently cannot run multiple OMNeT based simulations AND this is not yet ready to work properly")
+	public void testTravelMobilityLimitedRangeOmnet() throws AnnotationProcessorException, InterruptedException, DEECoException,
 			InstantiationException, IllegalAccessException, IOException {
 		OMNeTSimulation omnet = new OMNeTSimulation();
 				
@@ -52,23 +53,27 @@ public class VehicleTravelTest {
 		
 		// Node hosting vehicle A
 		MATSimVehicle agentA = new MATSimVehicle(); // MATSim agent with start position
-		DEECoNode nodeA = simulation.createNode(42, agentA, new PositionAware(0, 0)); // DEECO node with Id and agent as plug-in
-		Vehicle vehicleA = new Vehicle("Vehicle A", new CoordImpl(100000, 100000), agentA); // DEECO component controlling the vehicle
+		DEECoNode nodeA = simulation.createNode(42, agentA, new OMNeTBroadcastDevice(), new PositionAware(0, 0)); // DEECO node with Id and agent as plug-in
+		Vehicle vehicleA = new Vehicle("Vehicle A", new CoordImpl(1000, 2000), agentA); // DEECO component controlling the vehicle
 		nodeA.deployComponent(vehicleA);
 		nodeA.deployEnsemble(OtherVehicleEnsemble.class);
 
 		// Node hosting vehicle B
 		MATSimVehicle agentB = new MATSimVehicle(); // MATSim agent with start position
-		DEECoNode nodeB = simulation.createNode(45, agentB, new PositionAware(0, 100000)); // DEECO node with Id and agent as plug-in
-		Vehicle vehicleB = new Vehicle("Vehicle B", new CoordImpl(100000, 100000), agentB); // DEECO component controlling the vehicle
+		DEECoNode nodeB = simulation.createNode(45, agentB, new OMNeTBroadcastDevice(), new PositionAware(2000, 0)); // DEECO node with Id and agent as plug-in
+		Vehicle vehicleB = new Vehicle("Vehicle B", new CoordImpl(1000, 2000), agentB); // DEECO component controlling the vehicle
 		nodeB.deployComponent(vehicleB);
 		nodeB.deployEnsemble(OtherVehicleEnsemble.class);
 
 		// Simulate for specified time
-		simulation.start(7 * 60000);
+		simulation.start(10 * 60000);
 
-		// Check both cars reached the destination and know about each other
-		assertThat(log.getLog(), containsString("Vehicle A, pos: 4410 (1995, 2000), dst: 4410, speed: , otherPos: 4410 (1995, 2000)"));
-		assertThat(log.getLog(), containsString("Vehicle B, pos: 4410 (1995, 2000), dst: 4410, speed: , otherPos: 4410 (1995, 2000)"));
+		// Check if the vehicle initially have not known about each other as they were close
+		assertThat(log.getLog(), containsString("Vehicle A, pos: 5407 (400, 1105), dst: 4215, speed: , otherPos: UNKNOWN"));
+		assertThat(log.getLog(), containsString("Vehicle B, pos: 6187 (800, 1105), dst: 4215, speed: , otherPos: UNKNOWN"));
+		
+		// Check both cars reached the destination and know about each other (as they are close to each other)
+		assertThat(log.getLog(), containsString("Vehicle A, pos: 4215 (995, 2000), dst: 4215, speed: , otherPos: 4215 (995, 2000)"));
+		assertThat(log.getLog(), containsString("Vehicle B, pos: 4215 (995, 2000), dst: 4215, speed: , otherPos: 4215 (995, 2000)"));
 	}
 }
