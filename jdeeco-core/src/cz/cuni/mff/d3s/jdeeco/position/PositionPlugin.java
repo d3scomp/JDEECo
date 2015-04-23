c9c5d5024f316cace26a60893e88387ed5b2ae34
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-04-23 18:39:48 +0200

    Add basic commnets for PositionPlugin

--- jdeeco-core/src/cz/cuni/mff/d3s/jdeeco/position/PositionPlugin.java
+++ jdeeco-core/src/cz/cuni/mff/d3s/jdeeco/position/PositionPlugin.java
@@ -6,10 +6,20 @@ import java.util.List;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoContainer;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoPlugin;
 
+/**
+ * Position providing plug-in
+ * 
+ * This plug-in can work in two modes. In static mode the position is set in constructor and can be updated by explicit
+ * call to setStaticPosition. In provided mode the position is determined by PositionProvider registered with this
+ * plug-in. In order to avoid misunderstanding the provider can be set only once.
+ * 
+ * @author Vladimir Matena <matena@d3s.mff.cuni.cz>
+ *
+ */
 public class PositionPlugin implements DEECoPlugin, PositionProvider {
 	private Position staticPosition;
-	private PositionProvider provider;
-	
+	private PositionProvider provider = null;
+
 	public PositionPlugin(double x, double y) {
 		this(new Position(x, y, 0));
 	}
@@ -22,26 +32,40 @@ public class PositionPlugin implements DEECoPlugin, PositionProvider {
 		staticPosition = initialPosition;
 	}
 
+	/**
+	 * Sets position provider
+	 * 
+	 * Once set the provider cannot be removed or replaced and provides the current position of the node.
+	 * 
+	 * @param positionProvider
+	 */
 	public void setProvider(PositionProvider positionProvider) {
 		if (provider != null) {
 			throw new UnsupportedOperationException("Poisiton provider is already set, cannot add another one.");
 		}
-		
+
 		provider = positionProvider;
 	}
 
+	/**
+	 * Gets static position of the node
+	 * 
+	 * Static position is not provided by the provider
+	 * 
+	 * @return Static node position
+	 */
 	public Position getStaticPosition() {
 		return staticPosition;
 	}
-	
+
 	public void setStaticPosition(Position position) {
 		staticPosition = position;
 	}
-	
+
 	public void setStaticPosition(double x, double y) {
 		staticPosition = new Position(x, y, 0);
 	}
-	
+
 	public void setStaticPosition(double x, double y, double z) {
 		staticPosition = new Position(x, y, z);
 	}
