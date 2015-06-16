1b4085f29959f098d4045e4dde56c9989ff2de60
iridin <skoda@d3s.mff.cuni.cz>
2015-06-16 14:47:47 +0200

    Interface for the bumper.

--- jdeeco-ros/src/cz/cuni/mff/d3s/jdeeco/ros/Sensors.java
+++ jdeeco-ros/src/cz/cuni/mff/d3s/jdeeco/ros/Sensors.java
@@ -1,10 +1,14 @@
 package cz.cuni.mff.d3s.jdeeco.ros;
 
 import geometry_msgs.Point;
+import kobuki_msgs.BumperEvent;
+import nav_msgs.Odometry;
 
 import org.ros.message.MessageListener;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Subscriber;
+
+import cz.cuni.mff.d3s.jdeeco.ros.datatypes.Bumper;
 
 /**
  * Provides methods for obtaining sensed values passed through ROS. Subscription
@@ -20,11 +24,19 @@ public class Sensors extends TopicSubscriber {
 	 * The name of the odometry topic.
 	 */
 	private static final String ODOMETRY_TOPIC = "odom";
-
 	/**
 	 * The last position received in the odometry topic.
 	 */
 	private Point lastPosition;
+
+	/**
+	 * The name of the bumper topic.
+	 */
+	private static final String BUMPER_TOPIC = "/mobile_base/events/bumper";
+	/**
+	 * The bumper state.
+	 */
+	private Bumper bumper;
 
 	/**
 	 * The singleton instance of the {@link Sensors} class.
@@ -35,12 +47,13 @@ public class Sensors extends TopicSubscriber {
 	 * Private constructor enables the {@link Sensors} to be a singleton.
 	 */
 	private Sensors() {
+		bumper = Bumper.RELEASED;
 	}
 
 	/**
 	 * Provides the singleton instance of the {@link Sensors}.
 	 * 
-	 * @return the singleton instance of the {@link Sensors}. 
+	 * @return the singleton instance of the {@link Sensors}.
 	 */
 	public static Sensors getInstance() {
 		if (INSTANCE == null) {
@@ -57,31 +70,63 @@ public class Sensors extends TopicSubscriber {
 	 */
 	@Override
 	void subscribe(ConnectedNode connectedNode) {
-		//
-		// ODOMETRY topic
-		//
-		Subscriber<nav_msgs.Odometry> odometryTopic = connectedNode
-				.newSubscriber(ODOMETRY_TOPIC, nav_msgs.Odometry._TYPE);
-		odometryTopic
-				.addMessageListener(new MessageListener<nav_msgs.Odometry>() {
-					@Override
-					public void onNewMessage(nav_msgs.Odometry message) {
+		subscribeOdometry(connectedNode);
+		subscribeBumper(connectedNode);
+	}
 
-						lastPosition = message.getPose().getPose()
-								.getPosition();
-						/*
-						 * System.out.format("Position: %f,%f%n",
-						 * message.getPose().getPose().getPosition().getX(),
-						 * message.getPose().getPose().getPosition().getY());\
-						 * TODO: logging
-						 */
+	/**
+	 * Subscribe to the ROS odometry topic.
+	 * 
+	 * @param connectedNode
+	 *            The ROS node on which the DEECo node runs.
+	 */
+	private void subscribeOdometry(ConnectedNode connectedNode) {
+		Subscriber<Odometry> odometryTopic = connectedNode.newSubscriber(
+				ODOMETRY_TOPIC, Odometry._TYPE);
+		odometryTopic.addMessageListener(new MessageListener<Odometry>() {
+			@Override
+			public void onNewMessage(Odometry message) {
 
-						// TODO: make own implementation of Point to lose
-						// dependency on geometry_msgs artifact?
-					}
-				});
-		//
-		//
+				lastPosition = message.getPose().getPose().getPosition();
+				/*
+				 * System.out.format("Position: %f,%f%n",
+				 * message.getPose().getPose().getPosition().getX(),
+				 * message.getPose().getPose().getPosition().getY()); // TODO:
+				 * logging
+				 */
+			}
+		});
+	}
+
+	/**
+	 * Subscribe to the ROS bumper topic.
+	 * 
+	 * @param connectedNode
+	 *            The ROS node on which the DEECo node runs.
+	 */
+	private void subscribeBumper(ConnectedNode connectedNode) {
+		Subscriber<BumperEvent> bumperTopic = connectedNode.newSubscriber(
+				BUMPER_TOPIC, BumperEvent._TYPE);
+		bumperTopic.addMessageListener(new MessageListener<BumperEvent>() {
+			@Override
+			public void onNewMessage(BumperEvent message) {
+				byte state = message.getState();
+				if (state == BumperEvent.RELEASED) {
+					bumper = Bumper.RELEASED;
+					// System.out.println("released");
+				} else {
+					bumper = Bumper.fromByte(message.getBumper());
+					/*
+					 * if (message.getBumper() == BumperEvent.LEFT) {
+					 * System.out.println("left"); } if (message.getBumper() ==
+					 * BumperEvent.RIGHT) { System.out.println("right"); } if
+					 * (message.getBumper() == BumperEvent.CENTER) {
+					 * System.out.println("center"); }
+					 */
+				}
+				// TODO: log
+			}
+		});
 	}
 
 	/**
@@ -91,6 +136,15 @@ public class Sensors extends TopicSubscriber {
 	 */
 	public Point getPosition() {
 		return lastPosition;
+	}
+
+	/**
+	 * The bumper state published in the bumper topic.
+	 * 
+	 * @return the state of the bumper.
+	 */
+	public Bumper getBumper() {
+		return bumper;
 	}
 
 }
--- jdeeco-ros/src/cz/cuni/mff/d3s/jdeeco/ros/test/TestComponent.java
+++ jdeeco-ros/src/cz/cuni/mff/d3s/jdeeco/ros/test/TestComponent.java
@@ -7,6 +7,7 @@ import cz.cuni.mff.d3s.deeco.annotations.Process;
 import cz.cuni.mff.d3s.deeco.task.ParamHolder;
 import cz.cuni.mff.d3s.jdeeco.ros.Actuators;
 import cz.cuni.mff.d3s.jdeeco.ros.Sensors;
+import cz.cuni.mff.d3s.jdeeco.ros.datatypes.Bumper;
 import cz.cuni.mff.d3s.jdeeco.ros.datatypes.LedColor;
 import cz.cuni.mff.d3s.jdeeco.ros.datatypes.LedId;
 
@@ -26,24 +27,38 @@ public class TestComponent {
 	}
 
 	@Process
-	@PeriodicScheduling(period = 1000)
+	@PeriodicScheduling(period = 200)
 	public static void checkMessage(@InOut("msg") ParamHolder<String> message) {
-		System.out.println(message.value);
+		/*System.out.println(message.value);
 		if (Sensors.getInstance().getPosition() != null) {
 			message.value = String.format("[%f, %f]", Sensors.getInstance()
 					.getPosition().getX(), Sensors.getInstance().getPosition()
 					.getY());
-		}
+		}*/
 
 		//Actuators.getInstance().setVelocity(0.5,0);
-		Actuators.getInstance().setLed(LedId.LED1, led1);
-		Actuators.getInstance().setLed(LedId.LED2, led2);
-		if(led1 == LedColor.GREEN) led1 = LedColor.RED;
-		else led1 = LedColor.GREEN;
-		if(led2 == LedColor.GREEN) led2 = LedColor.RED;
-		else led2 = LedColor.GREEN;
+		Bumper bumper = Sensors.getInstance().getBumper();
+		//System.out.println(bumper);
+		switch(bumper){
+		case CENTER:
+			Actuators.getInstance().setLed(LedId.LED1, LedColor.RED);
+			Actuators.getInstance().setLed(LedId.LED2, LedColor.RED);
+			break;
+		case LEFT:
+			Actuators.getInstance().setLed(LedId.LED1, LedColor.RED);
+			Actuators.getInstance().setLed(LedId.LED2, LedColor.BLACK);
+			break;
+		case RIGHT:
+			Actuators.getInstance().setLed(LedId.LED1, LedColor.BLACK);
+			Actuators.getInstance().setLed(LedId.LED2, LedColor.RED);
+			break;
+		default:
+			Actuators.getInstance().setLed(LedId.LED1, LedColor.BLACK);
+			Actuators.getInstance().setLed(LedId.LED2, LedColor.BLACK);
+			break;
+		}
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(200);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
